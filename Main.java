2909fcf76d0692573bfdf66bba481d063a7284bf
unknown <Ljtron10@gmail.com>
2020-03-19 17:02:18 -0400

    First commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    
+    public static void main(String[] args){
+        Card card1 = new Card("10 black hearts", "idk", 10);
+        System.out.println(card1.toString());
+        Card card2 = new Card("2 black hearts", "idk", 10);
+        System.out.println(card2.toString());
+        Card card3 = new Card("3 black hearts", "idk", 10);
+        System.out.println(card3.toString());
+        Card card4 = new Card("4 black hearts", "idk", 10);
+        System.out.println(card4.toString());
+    }
+}
